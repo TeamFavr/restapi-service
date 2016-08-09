a6d4917481ee1e77728abe0f6be136b5f398155e
Charlie Thomas <charlie.thomas@attwoodthomas.net>
2016-08-09 21:27:30 +0100

    Add authentication

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,36 @@
-from flask import Flask, request
+import datetime
 
+from flask import Flask, current_app, jsonify, request
+
+from flask_jwt import JWT
+
+from .exceptions import CustomError
 from .services import USER_SERVICE
-from .utils import forward_request_to_service
+from .settings import SECRET_KEY
+from .utils import authenticate, forward_request_to_service, identity
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = SECRET_KEY
+app.config['JWT_AUTH_USERNAME_KEY'] = 'email'
+
+jwt = JWT(app, authenticate, identity)
+
+
+@jwt.jwt_payload_handler
+def payload_handler(identity):
+    print(identity)
+    iat = datetime.datetime.utcnow()
+    exp = iat + current_app.config.get('JWT_EXPIRATION_DELTA')
+    nbf = iat + current_app.config.get('JWT_NOT_BEFORE_DELTA')
+    identity = identity['id']
+    return {'exp': exp, 'iat': iat, 'nbf': nbf, 'identity': identity}
+
+
+@app.errorhandler(CustomError)
+def custom_error(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
 
 
 @app.route("/")
--- app/utils.py
+++ app/utils.py
@@ -1,5 +1,8 @@
 import requests
 
+from .exceptions import CustomError
+from .services import USER_SERVICE
+
 
 def forward_request_to_service(request, service, endpoint):
     url = "{}{}".format(service.host, endpoint)
@@ -11,3 +14,23 @@ def forward_request_to_service(request, service, endpoint):
                           headers=headers)
 
     return resp.text, resp.status_code
+
+
+def authenticate(email, password):
+    json_data = {'email': email, 'password': password}
+    response = requests.post("http://user/authenticate", json=json_data)
+    json_response = response.json()
+    if response.status_code >= 300:
+        raise CustomError(**json_response)
+
+    return json_response['user']
+
+
+def identity(payload):
+    url = "{}users/{}/".format(USER_SERVICE.host, payload['identity'])
+    response = requests.get(url)
+    json_response = response.json()
+    if response.status_code >= 300:
+        raise CustomError(**json_response)
+
+    return json_response['user']
