41dc39b3ad4835891c79b228edc91bd4cce7614d
Charlie Thomas <charlie.thomas@attwoodthomas.net>
2016-08-10 17:00:33 +0100

    Add friends listing route

--- app/endpoints/user.py
+++ app/endpoints/user.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, request
 
+from flask_jwt import jwt_required
+
 from app.utils import forward_request_to_service
 from app.services import USER_SERVICE
 
@@ -7,6 +9,7 @@ user_service = Blueprint("user_service", __name__)
 
 
 @user_service.route("/user")
+@jwt_required()
 def user_index():
     return forward_request_to_service(request, USER_SERVICE, "")
 
@@ -14,3 +17,9 @@ def user_index():
 @user_service.route("/signup", methods=["POST"])
 def user_signup():
     return forward_request_to_service(request, USER_SERVICE, "/signup")
+
+
+@user_service.route("/friends")
+@jwt_required()
+def friends():
+    return forward_request_to_service(request, USER_SERVICE, "/friends")
